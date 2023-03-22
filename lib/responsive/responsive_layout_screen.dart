3718082d76326faa9eaf2df666ad286932ecc6f8
rajat397 <rajat242003@gmail.com>
2023-03-22 23:35:50 +0530

    firebase setup

---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:uniconnect/util/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  const ResponsiveLayout(
+      {super.key,
+      required this.webScreenLayout,
+      required this.mobileScreenLayout});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > WebScreenSize) {
+          //web screen
+          return webScreenLayout;
+        }
+        //mobile screen
+        return mobileScreenLayout;
+      },
+    );
+  }
+}
---
+++ lib/util/dimensions.dart
@@ -0,0 +1,5 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+const WebScreenSize = 600;
