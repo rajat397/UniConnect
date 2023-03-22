50379706eb802118d0551ac2e0f8a6321492cec8
rajat397 <rajat242003@gmail.com>
2023-03-23 01:04:09 +0530

    login ui

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:uniconnect/widgets/text_field_input.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        width: double.infinity,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Flexible(child: Container(), flex: 2),
+          SvgPicture.asset(
+            'assets/logo.svg',
+            height: 64,
+          ),
+          const SizedBox(height: 64),
+          TextFieldInput(
+              textEditingController: emailController,
+              hintText: 'Enter your email',
+              textInputType: TextInputType.emailAddress),
+          TextFieldInput(
+            textEditingController: passwordController,
+            hintText: 'Enter your Password',
+            textInputType: TextInputType.text,
+            isPass: true,
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          InkWell(
+            child: Container(
+              child: const Text('Log in'),
+              width: double.infinity,
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              decoration: const ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(4),
+                    ),
+                  ),
+                  color: Colors.blue),
+            ),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          Flexible(child: Container(), flex: 2),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: const Text("Don't have an account?"),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8,
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  child: const Text(
+                    "Sign up",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ]),
+      )),
+    );
+  }
+}
