b08c6b66dd4d99390c807cf1e8ea2299b3212226
dhairya996 <dhairyabh96@gmail.com>
2023-04-04 01:12:14 +0530

    Carpool post upload added and error fixed. Post being stored in firebase

--- lib/screens/carpool_upload_post.dart
+++ lib/screens/carpool_upload_post.dart
@@ -1,3 +1,157 @@
+// // import 'dart:html';
+//
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:uniconnect/resources/firestore_methods.dart';
+// import 'package:uniconnect/util/colors.dart';
+// import 'package:uniconnect/util/utils.dart';
+//
+// import '../models/user.dart' as model;
+// import '../providers/providers.dart';
+// import '../widgets/text_field_input.dart';
+//
+// class Carpool_upload_post extends StatefulWidget {
+//   const Carpool_upload_post({super.key});
+//
+//   @override
+//   State<Carpool_upload_post> createState() => _Carpool_upload_postState();
+// }
+//
+// class _Carpool_upload_postState extends State<Carpool_upload_post> {
+//   final TextEditingController startController = TextEditingController();
+//   final TextEditingController destinationController = TextEditingController();
+//   final TextEditingController vehicleController = TextEditingController();
+//   final TextEditingController timeOfDepartureController =
+//       TextEditingController();
+//   final TextEditingController expectedPerHeadChargeController =
+//       TextEditingController();
+//
+//   @override
+//   void dispose() {
+//     super.dispose();
+//     startController.dispose();
+//     destinationController.dispose();
+//     vehicleController.dispose();
+//     timeOfDepartureController.dispose();
+//     expectedPerHeadChargeController.dispose();
+//   }
+//
+//   bool _isloading = false;
+//   void uploadPost() async {
+//     setState(() {
+//       _isloading = true;
+//     });
+//     try {
+//       model.User user = Provider.of<UserProvider>(context).getUser;
+//
+//       String res = await FirestoreMethods().uploadPost(
+//         start: startController.text,
+//         destination: destinationController.text,
+//         timeOfDeparture: timeOfDepartureController.text,
+//         vehicle: vehicleController.text,
+//         expectedPerHeadCharge: expectedPerHeadChargeController.text,
+//         username: user.username,
+//         uid: user.uid,
+//       );
+//       if (res == "success") {
+//         setState(() {
+//           _isloading = false;
+//         });
+//
+//         startController.clear();
+//         destinationController.clear();
+//         vehicleController.clear();
+//         timeOfDepartureController.clear();
+//         expectedPerHeadChargeController.clear();
+//         showSnackBar("Uploaded", context);
+//       } else {
+//         setState(() {
+//           _isloading = false;
+//         });
+//         showSnackBar(res, context);
+//       }
+//     } catch (e) {}
+//   }
+//
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: mobileBackgroundColor,
+//         leading: IconButton(
+//           icon: const Icon(Icons.arrow_back),
+//           onPressed: () {},
+//         ),
+//         title: const Text('Post to'),
+//         centerTitle: false,
+//         actions: [
+//           TextButton(
+//             onPressed: uploadPost,
+//             child: const Text(
+//               'Post',
+//               style: TextStyle(
+//                 color: Colors.blueAccent,
+//                 fontWeight: FontWeight.bold,
+//                 fontSize: 16,
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//       body: Column(
+//         children: [
+//           _isloading
+//               ? const LinearProgressIndicator()
+//               : const Padding(
+//                   padding: EdgeInsets.only(top: 0),
+//                 ),
+//                 const Divider(),
+//           TextFieldInput(
+//               textEditingController: startController,
+//               hintText: 'Enter the starting point of journey',
+//               textInputType: TextInputType.text),
+//           const SizedBox(
+//             height: 24,
+//           ),
+//           TextFieldInput(
+//               textEditingController: destinationController,
+//               hintText: 'Enter the destination',
+//               textInputType: TextInputType.text),
+//           const SizedBox(
+//             height: 24,
+//           ),
+//           TextFieldInput(
+//             textEditingController: timeOfDepartureController,
+//             hintText: 'Enter the time and Date of Departure',
+//             textInputType: TextInputType.datetime,
+//           ),
+//           const SizedBox(
+//             height: 24,
+//           ),
+//           TextFieldInput(
+//             textEditingController: vehicleController,
+//             hintText: 'Enter the vehicle ex- Cab or auto',
+//             textInputType: TextInputType.text,
+//           ),
+//           const SizedBox(
+//             height: 24,
+//           ),
+//           TextFieldInput(
+//             textEditingController: expectedPerHeadChargeController,
+//             hintText: 'Enter the Expected per Head Charge',
+//             textInputType: TextInputType.number,
+//           ),
+//           const SizedBox(
+//             height: 24,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 // import 'dart:html';
 
 import 'package:flutter/material.dart';
@@ -7,6 +161,7 @@ import 'package:uniconnect/util/colors.dart';
 import 'package:uniconnect/util/utils.dart';
 
 import '../models/user.dart' as model;
+import '../models/user.dart';
 import '../providers/providers.dart';
 import '../widgets/text_field_input.dart';
 
@@ -22,9 +177,9 @@ class _Carpool_upload_postState extends State<Carpool_upload_post> {
   final TextEditingController destinationController = TextEditingController();
   final TextEditingController vehicleController = TextEditingController();
   final TextEditingController timeOfDepartureController =
-      TextEditingController();
+  TextEditingController();
   final TextEditingController expectedPerHeadChargeController =
-      TextEditingController();
+  TextEditingController();
 
   @override
   void dispose() {
@@ -37,32 +192,36 @@ class _Carpool_upload_postState extends State<Carpool_upload_post> {
   }
 
   bool _isloading = false;
-  void uploadPost() async {
+  void uploadPost(
+      String uid,
+      String username,
+      ) async {
     setState(() {
       _isloading = true;
     });
     try {
-      model.User user = Provider.of<UserProvider>(context).getUser;
 
       String res = await FirestoreMethods().uploadPost(
-        start: startController.text,
-        destination: destinationController.text,
-        timeOfDeparture: timeOfDepartureController.text,
-        vehicle: vehicleController.text,
-        expectedPerHeadCharge: expectedPerHeadChargeController.text,
-        username: user.username,
-        uid: user.uid,
+        startController.text,
+        destinationController.text,
+        vehicleController.text,
+        timeOfDepartureController.text,
+
+        expectedPerHeadChargeController.text,
+        uid,
+
+        username,
       );
       if (res == "success") {
         setState(() {
           _isloading = false;
         });
-
-        startController.clear();
-        destinationController.clear();
-        vehicleController.clear();
-        timeOfDepartureController.clear();
-        expectedPerHeadChargeController.clear();
+        //
+        // startController.clear();
+        // destinationController.clear();
+        // vehicleController.clear();
+        // timeOfDepartureController.clear();
+        // expectedPerHeadChargeController.clear();
         showSnackBar("Uploaded", context);
       } else {
         setState(() {
@@ -70,13 +229,17 @@ class _Carpool_upload_postState extends State<Carpool_upload_post> {
         });
         showSnackBar(res, context);
       }
-    } catch (e) {}
+    } catch (e) {
+      showSnackBar(e.toString(), context);
+    }
   }
 
 
 
   @override
   Widget build(BuildContext context) {
+    final model.User? user =  Provider.of<UserProvider>(context).getUser;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: mobileBackgroundColor,
@@ -88,7 +251,10 @@ class _Carpool_upload_postState extends State<Carpool_upload_post> {
         centerTitle: false,
         actions: [
           TextButton(
-            onPressed: uploadPost,
+            onPressed:()=> uploadPost(
+              user!.uid,
+              user.username,
+            ),
             child: const Text(
               'Post',
               style: TextStyle(
@@ -105,9 +271,9 @@ class _Carpool_upload_postState extends State<Carpool_upload_post> {
           _isloading
               ? const LinearProgressIndicator()
               : const Padding(
-                  padding: EdgeInsets.only(top: 0),
-                ),
-                const Divider(),
+            padding: EdgeInsets.only(top: 0),
+          ),
+          const Divider(),
           TextFieldInput(
               textEditingController: startController,
               hintText: 'Enter the starting point of journey',
@@ -125,7 +291,7 @@ class _Carpool_upload_postState extends State<Carpool_upload_post> {
           TextFieldInput(
             textEditingController: timeOfDepartureController,
             hintText: 'Enter the time and Date of Departure',
-            textInputType: TextInputType.datetime,
+            textInputType: TextInputType.text,
           ),
           const SizedBox(
             height: 24,
@@ -141,7 +307,7 @@ class _Carpool_upload_postState extends State<Carpool_upload_post> {
           TextFieldInput(
             textEditingController: expectedPerHeadChargeController,
             hintText: 'Enter the Expected per Head Charge',
-            textInputType: TextInputType.number,
+            textInputType: TextInputType.text,
           ),
           const SizedBox(
             height: 24,
